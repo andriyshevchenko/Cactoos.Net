c7aaf357e92231230ad2fe1f1b100ca378f52573
Shevchenko Andriy <shewchenkoandriy@gmail.com>
2017-07-18 19:20:41 +0300

    DisposingScalar

---
+++ Cactoos.Net/Scalar/DisposingScalar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cactoos.Net.Scalar
+{
+    public class DisposingScalar<T> : IScalar<T>
+    {
+        private IDisposable _source;
+        private Func<IDisposable, T> _getValue;
+
+        public DisposingScalar(IDisposable source, Func<IDisposable, T> getValue)
+        {
+            _source = source;
+            _getValue = getValue;
+        }
+
+        public T Value()
+        {
+            using (_source)
+            {
+                return _getValue(_source);
+            }
+        }
+    }
+}
