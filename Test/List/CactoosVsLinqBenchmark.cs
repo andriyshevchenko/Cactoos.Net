fa528463299025d2e4a73c60958b397acc525ed9
Shevchenko Andriy <shewchenkoandriy@gmail.com>
2017-07-18 19:04:31 +0300

    remove unnecessary usings

--- Test/List/CactoosVsLinqBenchmark.cs
+++ Test/List/CactoosVsLinqBenchmark.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
-using System.Diagnostics;
 using Cactoos.List;
 using Cactoos.Scalar;
 
