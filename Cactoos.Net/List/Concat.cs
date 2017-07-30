56f2d85fc340988f7de973240367f6360fe7948b
Shevchenko Andriy <shewchenkoandriy@gmail.com>
2017-07-30 21:51:48 +0300

    Concatenated  enumerable.

---
+++ Cactoos.Net/List/Concat.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cactoos.List
+{
+    public class Concat<T> : IEnumerable<T>
+    {
+        private IEnumerable<T>[] _source;
+        private int _items;
+ 
+        public Concat(params IEnumerable<T>[] source)
+        {
+            _source = source;
+            _items = items;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var first = _source[0];
+            for (int i = 1; i < _source.Length; i++)
+            {
+                first = first.Concat(_source[i]);
+            }
+            return first.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
