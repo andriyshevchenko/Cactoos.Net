e804feede243f737f06b735def1daabe48d7dfc9
Shevchenko Andriy <shewchenkoandriy@gmail.com>
2017-08-06 16:55:18 +0300

    update packages

--- Cactoos.Net/Text/ParagraphText.cs
+++ Cactoos.Net/Text/ParagraphText.cs
@@ -1,10 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using InputValidation;
+using System;
 using System.Text;
 
-namespace Cactoos.Net.Text
+using static System.Collections.Generic.Create;
+
+namespace Cactoos.Text
 {
+    /// <summary>
+    /// A text with custom line width.
+    /// </summary>
     public class ParagraphText : IText
     {
+        private int _lineWidth;
+        private IText _source;
+
+        public ParagraphText(int lineWidth, IText source)
+        {
+            _lineWidth = lineWidth.CheckIfNatural("line width");
+            _source = source;
+        }
+
+        public ParagraphText(int lineWidth, string source) : this(lineWidth, new Text(source))
+        {
+
+        }
+
+        public string String()
+        {
+            var source = _source.String();
+            int wordStart = 0;
+            int wordStartBefore = 0;
+            int wordIdx = 0;
+            char[] word = new char[source.Length];
+            var newString = new StringBuilder();
+            for (int i = 0; i < source.Length; i++)
+            {
+                char @char = source[i];
+                word[wordIdx++] = @char;
+
+                if (@char == ' ')
+                {
+                    wordStart = i + 1;
+                    for (int j = 0; j < wordIdx; j++)
+                    {
+                        newString.Append(word[j]);
+                    }
+                    if (wordStartBefore + wordIdx + 1 >= _lineWidth)
+                    {
+                        newString.Append('\n');
+                    }
+                    wordIdx = 0;
+                    wordStartBefore = wordStart; 
+                }
+            }
+
+            return newString.ToString();
+        }
     }
 }
---
+++ Test/Text/ParagraphTextTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cactoos.Text;
+
+namespace Test.Text
+{
+    [TestClass]
+    public class ParagraphTextTest
+    {
+        [TestMethod]
+        public void should_insert_breaks()
+        {
+            Assert.AreEqual(
+                "I said a\n hip hop \nthe \nhippie \nthe \nhippie\nTo the\n hip hip\n" +
+                " hop and\n you \ndon't\n stop\nThe rock\n it to\n the bang\n bang \n" +
+                "boogie\nSay up\n jump \nthe \nboogie \nto the \nrhythm \nof the \n" +
+                "boogie, \nthe beat\n",
+                new ParagraphText(
+                    10,
+                    "I said a hip hop the hippie the hippie " +
+                    "To the hip hip hop and you don't stop " +
+                    "The rock it to the bang bang boogie " +
+                    "Say up jump the boogie to the rhythm of the boogie, the beat"
+                ).String()
+            );
+        }
+    }
+}
