b4fa1ea2e4471f24a744b87bef9a593ac74da2a0
Shevchenko Andriy <shewchenkoandriy@gmail.com>
2017-08-21 13:26:57 +0300

    LazyAsyncScalar

---
+++ Cactoos.Net/Scalar/Async/LazyAsyncScalar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cactoos.Scalar.Async
+{
+    public struct LazyAsyncScalar<T> : IAsyncScalar<T>
+    {
+        private Func<Task<T>> _source;
+        private T _value;
+        bool activated;
+
+        public LazyAsyncScalar(Func<Task<T>> source)
+        {
+            activated = false;
+            _value = default(T);
+            _source = source;
+        }
+
+        public async Task<T> Value()
+        {
+            if (activated)
+            {
+                return _value;
+            }
+            activated = true;
+            return _value = await _source();
+        }
+    }
+}
